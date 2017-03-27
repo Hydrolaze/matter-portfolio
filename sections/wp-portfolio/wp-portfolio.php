0f00c1f46567d8018dea6da70223e8ac3ded3407
Aaron Clement <hydrolaze@gmail.com>
2017-03-26 20:05:44 -0700

    Layout Changes
    >Adjusted the layout of the wordpress portfolio page.

--- sections/wp-portfolio/wp-portfolio.php
+++ sections/wp-portfolio/wp-portfolio.php
@@ -8,15 +8,15 @@
     
     <div class="copy">
     
-    <?php single_photo($section_name, 'http://clement.matmcc.org/portfolio/','wp-portfolio-screenshot.png', 720, 'centre', 'Wordpess let me manage posts and content easily, and plugins gave me easy interactivity.'); ?>
+    <?php single_photo($section_name, 'http://clement.matmcc.org/portfolio/','wp-portfolio-screenshot.png', 720, 'centre', 'Wordpress let me manage posts and content easily, and plugins gave me easy interactivity.'); ?>
     
     <p>To aid me in my search for a job, I created a Wordpress portfolio to showcase my work. After installing Wordpress on my server and linking the MySQL database, I set out to find a suitable theme to use.</p>
     
-    <?php single_photo($section_name, 'https://wordpress.com/themes/blask/', 'blask-theme.jpg', 360, 'right', 'Blask by Automattic.'); ?>
+    <?php single_photo($section_name, 'https://wordpress.com/themes/blask/', 'blask-theme.jpg', 300, 'right', 'Blask by Automattic.'); ?>
     
     <p>I settled on Blask, a minimalistic theme that would be easy to modify for my purposes. I defined a child theme and set about modifying it, changing some fonts, background images, and placement of some elements.</p>
 
-    <?php single_photo($section_name, false, 'limb-gallery-screenshot.png', 360, 'left', 'Limb Gallery, a versatile plugin I used to create galleries.'); ?>
+    <?php single_photo($section_name, false, 'limb-gallery-screenshot.png', 300, 'left', 'Limb Gallery, a versatile plugin I used to create galleries.'); ?>
     
     <p>I also implemented several plugins to add functionality to my portfolio such as contact forms and image galleries. I also used a development plugin called WP Example Content to let me see what my portfolio looked like with content before I took the time to add it in.</p>
 
