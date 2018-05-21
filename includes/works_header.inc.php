1789dd93a2403b78de4c4671ac91a84eeaaca6a7
Aaron Clement <hydrolaze@gmail.com>
2018-05-20 21:50:02 -0700

    XML fixes
    > Corrected my bad XML.
    > Fixed an incorrect link in works_header.inc.php.

--- includes/works_header.inc.php
+++ includes/works_header.inc.php
@@ -1,4 +1,24 @@
-<?php include(__DIR__.'/works_list.inc.php'); ?> 
+
+
+<?php 
+
+//$key_is_valid = false;
+//if(isset($_GET['key'])) {
+//    include(__DIR__.'../custom-queue/queues.php');
+//    if(isset($queues[$_GET['key']])) {
+//        $key_is_valid = true;
+//    };
+//};
+//
+//if ($key_is_valid) {
+//    include(__DIR__.'/full_works_list.inc.php');
+//} else {
+//    include(__DIR__.'/works_list.inc.php');
+//};
+
+include(__DIR__.'/full_works_list.inc.php');
+
+?> 
 
 <section id="<?php echo $section_name; ?>">
     <img src="sections/<?php echo $section_name."/".$section_name; ?>_tableau.jpg" class="tableau">
